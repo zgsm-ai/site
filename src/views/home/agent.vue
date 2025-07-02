65d9d49613877dd50432f9c8e793060953e9bbbf
xjm <2413294257@qq.com>
2025-07-02 16:39:32 +0800

    feat: adds new image features and user guides for Shenma CLI

--- src/views/home/agent.vue
+++ src/views/home/agent.vue
@@ -4,10 +4,12 @@
     <div class="w-full h-[702px] rounded-[20px] mt-10 gradient-border">
       <img class="rounded-[20px]" :src="demo" />
     </div>
-    <div class="flex gap-[22px] flex-wrap mt-6">
+    <div class="flex gap-6 flex-wrap mt-6">
       <div v-for="(item, i) in featureList" :key="i">
-        <div class="w-[418px] h-[420px]">
-          <img :src="item" />
+        <div class="w-[638px] h-[400px] relative">
+          <img :src="item.img" />
+          <img v-if="item.needGuide" :src="t('home.agent.guide')" class="absolute top-8 right-4 cursor-pointer"
+            @click="openWordPreview" />
         </div>
       </div>
     </div>
@@ -15,6 +17,8 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import EnGuidePdf from '@/assets/guide_en.pdf'
+import ZhGuidePdf from '@/assets/guide_zh.pdf'
 
 export default defineComponent({
   name: 'AgentView',
@@ -25,13 +29,40 @@ import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import PageTitle from '@/components/PageTitle.vue'
 
-const { t } = useI18n()
+const { t, locale } = useI18n()
+
 const featureList = computed(() => [
-  t('home.agent.feature01'),
-  t('home.agent.feature02'),
-  t('home.agent.feature03'),
+  {
+    img: t('home.agent.feature01'),
+  },
+  {
+    img: t('home.agent.feature02'),
+  },
+  {
+    img: t('home.agent.feature03'),
+    needGuide: true
+  },
+  {
+    img: t('home.agent.feature04'),
+  }
 ])
 const demo = computed(() => t('home.agent.demo'))
+
+const isZh = computed(() => locale.value === 'zh')
+
+const openWordPreview = () => {
+  const link = document.createElement('a')
+
+  link.href = isZh.value ? ZhGuidePdf : EnGuidePdf
+  link.target = '_blank'
+  link.rel = 'noopener noreferrer'
+
+  document.body.appendChild(link)
+
+  link.click()
+
+  document.body.removeChild(link)
+}
 </script>
 <style lang="less" scoped>
 .gradient-border {
